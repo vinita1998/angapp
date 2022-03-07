0dc90b2c8d07f4899c9917d5b7a328c925a5f17f
Vinita <zugarevinita@gmail.com>
2022-03-07 16:35:02 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'angapp';
-}
+import { Component } from '@angular/core';      
+      
+@Component({      
+  selector: 'app-root',      
+  templateUrl: './app.component.html',      
+  styleUrls: ['./app.component.css']      
+})      
+export class AppComponent {      
+  title = 'Template Driven-Forms';      
+  Register(regForm:any){      
+    debugger;      
+    var firstname=regForm.controls.firstname.value;      
+    var lastname=regForm.controls.lastname.value;      
+    var email=regForm.controls.lastname.value;      
+    console.log(regForm);      
+  }      
+}  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';      
+import { NgModule } from '@angular/core';      
+import { AppComponent } from './app.component';      
+import {FormsModule} from '@angular/forms'      
+@NgModule({      
+  declarations: [      
+    AppComponent      
+  ],      
+  imports: [      
+    BrowserModule,FormsModule      
+  ],      
+  providers: [],      
+  bootstrap: [AppComponent]      
+})      
+export class AppModule { }    
